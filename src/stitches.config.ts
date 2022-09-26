acb417a431528075213bb4bb20455c6b01978f31
Matheus Gabriel <matheus.gabriel.couto@hotmail.com>
2022-09-26 12:08:06 -0400

    adding stitches

---
+++ src/stitches.config.ts
@@ -0,0 +1,18 @@
+import { createStitches } from '@stitches/react'
+
+export const {} = createStitches({
+  theme: {
+    colors: {
+
+    },
+    fonts: {
+      sm: '0.85rem',
+      md: '1rem',
+      lg: '1.2rem'
+    },
+  }, media: {
+    bp1: '(min-width: 640px)',
+    bp2: '(min-width: 768px)',
+    bp3: '(min-width: 1024px)',
+  }
+})
